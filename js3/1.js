2c55aedc1205c330c391f861fd19d9c1b83a71fd
bilgedemirkaya <59152610+bilgedemirkaya@users.noreply.github.com>
2020-12-29 23:35:40 +0300

    js3

--- js3/1.js
+++ js3/1.js
@@ -3,9 +3,16 @@
  * @param {array} arr {object} obj
  * @returns {array} arr
  */
-
-const solution = (arr, obj) => {
-  return []
+// with recursion
+const solution = (arr, obj, i=0, newArr=[]) => {
+  let keys = Object.keys(obj)
+  if (i > keys.length) {
+    return newArr
+  }
+  if (keys.includes(arr[i])) {
+    newArr.push(obj[arr[i]])
+  }
+  return solution(arr,obj, i+1, newArr)
 }
 
 module.exports = {
