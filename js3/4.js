1165350eab86f3b660d1de4f8abb1a126cd3b3f6
bilgedemirkaya <59152610+bilgedemirkaya@users.noreply.github.com>
2020-12-31 20:05:35 +0300

    js3/4

--- js3/4.js
+++ js3/4.js
@@ -6,8 +6,15 @@
  * @returns {boolean}
  */
 
-const solution = (arr, num) => {
-  return true
+const solution = (arr, num, i=0) => {
+  if ( i >= arr.length ) {
+    return false
+  }
+  arr.splice(i,1)
+  if (arr.includes(num - arr[i])) {
+      return true
+  }
+  return solution(arr, num, i+1)
 }
 
 module.exports = {
