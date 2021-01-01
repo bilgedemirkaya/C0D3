21989f57f95551dc480b511f5b6a0a9bb7735d34
bilgedemirkaya <59152610+bilgedemirkaya@users.noreply.github.com>
2021-01-01 23:33:42 +0300

    6.js

--- js3/6.js
+++ js3/6.js
@@ -4,8 +4,23 @@
  * @returns {array}
 */
 
-const solution = (arr) => {
-  return []
+const val = arr[i]
+const keys = Object.keys(hashmap)
+
+const solution = (arr, i=0, hashmap= {}) => {
+  const val = arr[i]
+  const keys = Object.keys(hashmap)
+  
+  if ( i >= arr.length ) {
+    return keys.reduce((acc, key) => { 
+      if (hashmap[key]) {
+          acc.push(parseInt(key))
+      }
+      return acc
+      },[])
+  }
+  hashmap.hasOwnProperty(val) ? hashmap[val] = true  : hashmap[val] = false
+  return solution(arr, i+1, hashmap)
 }
 
 module.exports = {
