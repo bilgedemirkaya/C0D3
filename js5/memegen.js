60d6993e37899883f25bc10160f22857a4f70e7f
bilgedemirkaya <59152610+bilgedemirkaya@users.noreply.github.com>
2021-02-07 23:46:46 +0300

    Add router

--- js5/memegen.js
+++ js5/memegen.js
@@ -1,22 +1,11 @@
-const express = require('express');
-const app = express();
-const fs = require('fs');
-app.use(express.static('public'));
-const Jimp = require('jimp');
+const express = require('express')
+const app = express()
+const fs = require('fs')
+app.use(express.static('public'))
+const Jimp = require('jimp')
+const router = express.Router()
 
-app.get('/', (req, res) => {
-  fs.readdir(`${__dirname}/public`, (err, files) => {
-    res.send( files.reduce((acc, f) => {
-      return acc + `
-      <div>
-         <a href="/${f}" target="__blank">${f}</a>
-      </div>
-      `
-    }, ''))
-  })
-})
-
-app.get('/memegen/api/:text', async (req, res) => {
+router.get('/memegen/api/:text', async (req, res) => {
   const text = req.params.text
   let { blur, src, black } = req.query
   
