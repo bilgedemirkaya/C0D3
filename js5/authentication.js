0f49a2497160a0bfa92ca205829c013df4865c6c
Bilge Demirkaya <bilgedemirkaya00@hotmail.com>
2021-03-03 21:35:26 +0300

    fix authentication

--- js5/authentication.js
+++ js5/authentication.js
@@ -1,16 +1,18 @@
 const express = require('express')
 const router = express.Router()
-
+ 
 var path = require('path')
 
 const views = path.join(__dirname, 'views')
 
-const usersObject = {users: []}
 const jwt = require('jsonwebtoken')
 const bcrypt = require('bcrypt')
 const saltRounds = 10
 const { v4: uuidv4 } = require('uuid')
 
+const usersById = {}
+const usersByUsername = {}
+const usersByEmail = {}
 
 router.get('/', (req, res) => {
   res.sendFile(path.join(views, 'auth.html'))
@@ -24,64 +26,36 @@ router.post('/api/users', (req, res, next) => {
   const passwordstr = Buffer.from(password, 'base64').toString()
 
   if (passwordstr.length < 5 ) 
-    res.status(400).json({error: 'Password should be at least 5 characters.'})
+    return res.status(400).json({error: 'Password should be at least 5 characters.'})
   if (!username.match(/\w+$/)) 
     return res.status(400).json({error: 'Username must be alphanumeric'})
   if (!email.match(/^\S+@\S+$/))
     return res.status(400).json({error: 'Invalid email'})
 
-
-  let isUsernameUnique = true
-  let isEmailUnique = true
-
-  usersObject['users'].map( (user) => {
-    if(user['username'] === username) isUsernameUnique = false
-    if(user['email'] === email) isEmailUnique = false
-  })
-
-  if (!isUsernameUnique) 
+  if (usersByUsername[username]) 
     return res.status(400).json({error: 'Username taken'})
-  if (!isEmailUnique)
+  if (usersByEmail[email])
     return res.status(400).json({error: 'Email taken'})
   
   // hash password
-  const hash = bcrypt.hashSync(password, saltRounds)
-
-  const data = {
-    id : uuidv4(),
-    name,
-    username,
-    email,
-    hash
-  }
-
+  bcrypt.hash(password, saltRounds, function(err, hash) {
+    if (err) console.log(err)
+    const data = {
+      id : uuidv4(),
+      name,
+      username,
+      email,
+      hash
+    }
+    
   data["jwt"]= jwt.sign({ userId: data.id }, 'secret code', { expiresIn: '1800s' })
 
-  usersObject['users'].push(data) 
-  res.json(data)
-})
+  usersById[data.id] = data
+  usersByUsername[data.username] = data
+  usersByEmail[data.email] = data
 
-// get users list
-router.get('/api/users', (req, res) => {
-  res.json(usersObject)
-})
-
-// create a new session
-router.post('/api', async (req, res) => {
-  const {username, password} = req.body
-  let user
-  usersObject['users'].map( u => {
-   if (u['username'] === username) // there is a user with that username
-      user = u 
+  res.status(202).send({jwt: data.jwt})
   })
-  if(!user) 
-    return res.status(400).json({ error: 'There is no account with that username.' })
-
- const match =  await bcrypt.compare(password, user.hash)
- if (!match) 
-      return res.status(400).json({ error: 'Invalid password.' })
-
-  return res.json({ jwt: user.jwt })
 })
   
 
@@ -89,9 +63,23 @@ router.post('/api', async (req, res) => {
 // You must accept a json web token in the header field.
 router.get('/api', (req, res) => {
   const token = req.headers.authorization.split(' ')[1]
-  const user = jwt.decode(token)
-  if (!user) res.json({error: 'not authorized'})
-  else res.json({jwt: token})
+  user = jwt.decode(token)
+  if (user === null) res.status(403).json({error: 'not authorized'})
+  else res.status(202).json({jwt: token})
+})
+
+// create a new session
+router.post('/api', async (req, res) => {
+  const {username, password} = req.body
+  const user = usersByUsername[username]
+  if(!user)
+    return res.status(400).json({ error: 'There is no account with that username.' })
+
+ const match = await bcrypt.compare(password, user.hash)
+ if (!match) 
+      return res.status(400).json({ error: 'Invalid password.' }) 
+
+  return res.json({ jwt: user.jwt })
 })
 
 module.exports = router
