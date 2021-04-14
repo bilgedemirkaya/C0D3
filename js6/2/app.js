15a46526c5c48ef5da5bca0722845406f7d9fda7
bilge <bilgedemirkaya00@hotmail.com>
2021-04-14 10:36:46 +0300

    app

--- js6/2/app.js
+++ js6/2/app.js
@@ -1,28 +1,50 @@
 const express = require('express')
-const app = express()
 
-const { ApolloServer } = require('apollo-server')
+const { ApolloServer } = require('apollo-server-express')
 const typeDefs = require('./schema')
 const LessonAPI = require('./datasource/lessons')
 const PokemonAPI = require('./datasource/pokemons')
 const resolvers = require('./resolvers')
+const session = require('express-session')
+var path = require('path')
+const views = path.join(__dirname, 'views')
 
-const server = new ApolloServer({ 
-    typeDefs,
-    resolvers,
-    dataSources: () => ({
-      LessonAPI: new LessonAPI(),
-      PokemonAPI: new PokemonAPI()
-    })
-});
-app.use(express.static('public'))
+async function startApolloServer() {
+  const app = express()
 
-//app.listen(process.env.PORT || 8123)
-server.listen().then(() => {
-    console.log(`
-      Server is running!
-      Listening on port 4000
-      Explore at https://studio.apollographql.com/dev
-    `);
+  const server = new ApolloServer({ 
+      typeDefs,
+      resolvers,
+      dataSources: () => ({
+        LessonAPI: new LessonAPI(),
+        PokemonAPI: new PokemonAPI(),
+      }),
+      context: (({ req }) => {
+        if (!req.session.user) {
+          req.session.user = {}
+        }
+        return { req } 
+      }),
   });
+  await server.start();
+
+  app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true
+  }))
   
+  app.get('/', (req, res) => {
+    res.sendFile(path.join(views, 'addLessons.html'))
+  })
+
+  app.use(express.static('public'))
+  server.applyMiddleware({ app })
+
+
+  await new Promise(resolve => app.listen({ port: 4000 }, resolve));
+  console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`);
+  return { server, app };
+}
+
+startApolloServer() 
--- js6/2/schema.js
+++ js6/2/schema.js
@@ -6,24 +6,40 @@ const typeDefs = gql`
     title: String,
     challenges: [challenges]
   }
+
   type challenges {
         id: ID!
         title: String
         description: String
     }
+
     type Pokemon {
         name: String
         image: String
     }
+
     type BasicPokemon {
         name: String
     }
     
+    type User {
+        name: String
+        image: String
+        lessons: [Lesson]
+    }
+    
+    type Mutation {
+        enroll(title: String!): User
+        unenroll(title: String!): User
+    }
+
     type Query {
         lessons: [Lesson]
-        pokemons: [Pokemon]
         search(str: String!): [BasicPokemon]
         getPokemon(str: String!): Pokemon
+        user: User
+        login(pokemon: String!): User
+        pokemons: [Pokemon]
     }
 `;
 
